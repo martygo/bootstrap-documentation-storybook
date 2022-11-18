963ef50c8299567527c1be8b5572352561bce9ad
Martins Gouveia <gouveiamartins6@gmail.com>
2022-11-18 22:19:39 +0100

    🎨 created my custom theme

--- .storybook/manager.cjs
+++ .storybook/manager.cjs
@@ -1,6 +1,15 @@
 import { addons } from "@storybook/addons";
-import { themes } from "@storybook/theming";
+import { create } from "@storybook/theming";
+
+export const customTheme = create({
+	base: "dark",
+	brandTitle: "Stories of Bootstrap",
+	brandUrl: "https://example.com",
+	brandImage:
+		"https://getbootstrap.com/docs/5.0/assets/brand/bootstrap-logo.svg",
+	brandTarget: "_self",
+});
 
 addons.setConfig({
-	theme: themes.dark,
+	theme: customTheme,
 });
