e1f493cf8da1aeb7aa01e5212e508dd6703bd55f
Martins Gouveia <gouveiamartins6@gmail.com>
2022-11-18 22:48:24 +0100

    ✨ add preview html

--- .storybook/preview.cjs
+++ .storybook/preview.cjs
@@ -3,6 +3,13 @@ import "bootstrap-icons/font/bootstrap-icons.css";
 
 export const parameters = {
 	actions: { argTypesRegex: "^on[A-Z].*" },
+	html: {
+		prettier: {
+			tabWidth: 4,
+			useTabs: false,
+			htmlWhitespaceSensitivity: "strict",
+		},
+	},
 	controls: {
 		matchers: {
 			color: /(background|color)$/i,
